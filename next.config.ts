9a1b040b40740737f77ec2261fc505ebb66e51db
Ben DiMarco <bendimarco@Bens-MacBook-Pro-2.local>
2025-02-16 23:45:06 -0500

    NextJS deployment setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/ShelloOnboarding",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
